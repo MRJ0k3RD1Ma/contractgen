368b5c66a80fa9f8f74cdef383acbbb3cd69889b
Dilmurod Allabergenov <mr.j0k3r.d1ma@gmail.com>
2022-06-11 18:21:26 +0500

    sdf

--- backend/controllers/GenController.php
+++ backend/controllers/GenController.php
@@ -3,6 +3,9 @@
 namespace backend\controllers;
 
 use rudissaar\fpdf\FPDF;
+use Spipu\Html2Pdf\Exception\ExceptionFormatter;
+use Spipu\Html2Pdf\Exception\Html2PdfException;
+use Spipu\Html2Pdf\Html2Pdf;
 use yii\filters\Cors;
 use yii\rest\Controller;
 use yii\web\Response;
@@ -39,10 +42,24 @@ class GenController extends Controller {
     }
 
     public function generate($data){
-        $pdf = new FPDF('P','mm','A4');
-        $pdf->AddPage();
-        $pdf->SetFont('times', 'B', 15);
-        $pdf->Cell(40, 10, 'Hello World');
-        $pdf->Output('D', Yii::$app->basePath.'/web/tmp/contract.pdf');
+
+        try {
+
+            $html2pdf = new Html2Pdf('P', 'A4', 'ru',true,'UTF-8',[0,0,0,0]);
+            $html2pdf->pdf->setDisplayMode('fullpage');
+            $html2pdf->writeHTML(
+                $this->render('contract')
+            );
+            error_reporting(0);
+            $html2pdf->createIndex('',32,12,false,1,'helvetica');
+
+            $html2pdf->output('contact.pdf','D');
+            exit;
+
+        }catch (Html2PdfException $e){
+            $formatter = new ExceptionFormatter($e);
+            return $formatter->getMessage();
+        }
+
     }
 }
